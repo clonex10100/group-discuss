c3ddda53f61e752396f46dc05d3844c901157a7e
clonex10100 <clonex10100@gmail.com>
2020-02-15 23:58:57 -0500

    added devise basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'topics#index'
   #The creation of topics is nested under groups, but topic show pages are top level with comments nested under them to avoid overly long urls
