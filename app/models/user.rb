8c5c5575f2c5b0ea785ca1c12ca9244503a56b22
clonex10100 <clonex10100@gmail.com>
2020-02-16 00:16:52 -0500

    switched devise authentication to use usernames instead of emails

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable
+  has_many :topics
+  has_many :comments
+end
