37ae8d0be0f34327fe8675b30236af87d3656687
clonex10100 <clonex10100@gmail.com>
2020-02-15 23:24:12 -0500

    gutted old authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  helper_method :current_user_id
-  helper_method :logged_in?
-  def current_user_id
-    session[:user_id]
-  end
-  def current_user
-    User.find(current_user_id)
-  end
-
-  def logged_in?
-    !!session[:user_id]
-  end
 end
