71fa742fa48669b56709633b00134b42492bdc4d
clonex10100 <clonex10100@gmail.com>
2020-02-16 22:48:28 -0500

    added new group functionality

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,22 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @topics = @group.topics.page params[:page]
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_path(@group)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'topics#index'
   #The creation of topics is nested under groups, but topic show pages are top level with comments nested under them to avoid overly long urls
-  resources :groups, only: [:index, :show] do
+  resources :groups, only: [:index, :show, :new, :create] do
     resources :topics, only: [:new, :create]
   end
 
