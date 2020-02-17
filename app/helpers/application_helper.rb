17161f0d0969953670354138b4514ab01465e3a9
clonex10100 <clonex10100@gmail.com>
2020-02-17 18:07:33 -0500

    factored out error display into a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_errors_for(model)
+    out = if model.errors.any?
+      content_tag(:div, class: ["error_explanation"]) do
+        content_tag(:h2, "#{pluralize(model.errors.count, 'error')} prohibited this topic from being posted:") + content_tag(:ul, model.errors.full_messages.reduce('') { |str, msg| str += " " + content_tag(:li, msg) }.html_safe )
+      end
+    end
+  end
 end
