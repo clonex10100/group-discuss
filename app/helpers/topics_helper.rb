c4faf0f08a837de106a7a88a626392178ad7fc1a
clonex10100 <clonex10100@gmail.com>
2020-02-17 19:10:48 -0500

    Moved preview from topic helper to topic model

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,2 @@
 module TopicsHelper
-  def preview(text)
-    text.length > 150 ? text[0, 150] + "..." : text
-  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,4 +8,7 @@ class Topic < ApplicationRecord
   scope :date_ascending, -> { order(created_at: :asc) }
   scope :comments_descending, -> { left_outer_joins(:comments).group('topics.title').order('count(comments.content) desc') }
   scope :comments_ascending, -> { left_outer_joins(:comments).group('topics.title').order('count(comments.content) asc') }
+  def preview
+    content.length > 150 ? content[0, 150] + "..." : content
+  end
 end
