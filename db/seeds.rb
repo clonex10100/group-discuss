7be19203f7d76df501142cdbbf2c290f486ad998
clonex10100 <clonex10100@gmail.com>
2020-02-24 17:47:06 -0500

    Added random seeds using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = 3.times.reduce([]) { |array, i| array << User.create(username: Faker::Internet.unique.username , password: 'password') }
+
+groups = 5.times.reduce([]) { |array, i| array << Group.create(name: Faker::Lorem.word) }
+50.times do
+  users.each do |user|
+    topic = user.topics.build(title: Faker::Lorem.sentences(number: 1)[0], content: Faker::Lorem.sentences(number: 4).join(' '), group: groups[rand(3)])
+    topic.save
+  end
+end
+
+200.times do
+  Topic.all[rand(Topic.all.length)].comments.build(user: users[rand(3)], content: Faker::Lorem.sentences(number: 3).join(' ')).save
+end
+
+300.times do
+  value = rand(10) > 1 ? 1 : -1
+  users[rand(3)].votes.build(topic: Topic.all[rand(Topic.all.length)], value: value).save
+end
